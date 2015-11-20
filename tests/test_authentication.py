c182566a228c709c21922edfcada6393080c52d0
Adam Dangoor <adamdangoor@gmail.com>
2015-11-20 19:40:45 +0000

    Removed template tests

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -2,20 +2,17 @@ import os
 import unittest
 import tempfile
 
-import authentication as flaskr
+import authentication
 
-class FlaskrTestCase(unittest.TestCase):
+class SignupTests(unittest.TestCase):
+    """
+    Signup tests.
+    """
 
-    def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        with flaskr.app.app_context():
-            flaskr.init_db()
-
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+class LoginTests(unittest.TestCase):
+    """
+    Tests for logging in.
+    """
 
 if __name__ == '__main__':
     unittest.main()
