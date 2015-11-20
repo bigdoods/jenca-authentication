787df265819a88dc71f8a32cc0e752a1e795cddd
Adam Dangoor <adamdangoor@gmail.com>
2015-11-20 20:21:14 +0000

    A couple of test definitions

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,13 +1,22 @@
-import os
 import unittest
 import tempfile
 
-import authentication
+from authentication import authentication
 
 class SignupTests(unittest.TestCase):
     """
     Signup tests.
     """
+
+    def test_signup(self):
+        """
+        Test that a valid signup request returns an OK status.
+        """
+        test_app = authentication.app.test_client()
+
+    def test_missing_username(self):
+        pass
+
 
 class LoginTests(unittest.TestCase):
     """
