5bdbdfa94d06065c0536b684e6694b94ad80047b
Adam Dangoor <adamdangoor@gmail.com>
2015-11-21 14:22:05 +0000

    Use email variable name where appropriate

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -5,16 +5,17 @@ app = Flask(__name__)
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.form['email']
+    email = request.form['email']
     password = request.form['password']
-    response_content = {'email': username, 'password': password}
+    response_content = {'email': email, 'password': password}
     return jsonify(response_content), codes.OK
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    username = request.form['email']
+    email = request.form['email']
     password = request.form['password']
-    response_content = {'email': username, 'password': password}
+    user = User(email=email, password=password)
+    response_content = {'email': email, 'password': password}
     return jsonify(response_content), codes.CREATED
 
 if __name__ == '__main__':
