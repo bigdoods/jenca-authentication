8c51c58b867552263bb0c150581803c34733271b
Adam Dangoor <adamdangoor@gmail.com>
2015-11-26 23:45:12 +0000

    Use chosen path url

--- authentication/tests/test_authentication.py
+++ authentication/tests/test_authentication.py
@@ -46,8 +46,8 @@ class SignupTests(unittest.TestCase):
                 storage_db.create_all()
             client = storage_app.test_client()
             NEW_USER_DATA = {'email': 'alice@example.com', 'password_hash': '123abc'}
-            response = client.post('/users', content_type='application/json', data=json.dumps(NEW_USER_DATA))
-            import pdb; pdb.set_trace()
+            response = client.post(request.path_url, content_type='application/json', data=json.dumps(NEW_USER_DATA))
+            # import pdb; pdb.set_trace()
             return (
                 response.status_code,
                 {key: value for (key, value) in response.headers},
