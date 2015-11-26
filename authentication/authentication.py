71a5cf6828552b2ca6bec2b5efd9f5f8cce3a554
Adam Dangoor <adamdangoor@gmail.com>
2015-11-26 21:49:17 +0000

    Try a session type for secrets

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -53,7 +53,11 @@ class User(UserMixin):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET', 'secret')
+app.config['SESSION_TYPE'] = 'memcached'
+
+# TODO use environment variables for these
+app.secret_key = 'secret'
+app.config['SECRET_KEY'] = 'secret'
 bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
